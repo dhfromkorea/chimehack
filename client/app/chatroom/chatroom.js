48c1a02f7cd319197a7ee04f73e55bf4a4c25ebe
dhfromkorea <dhllee424@gmail.com>
2015-07-11 23:20:48 -0700

    implemented scroll down when a new message is sent

--- client/app/chatroom/chatroom.js
+++ client/app/chatroom/chatroom.js
@@ -1,39 +1,44 @@
-angular.module('anonichat.Chatroom', [
-  'firebase',
-  'ngRoute'
-])
+(function() {
+    angular.module('anonichat.Chatroom', [
+        'firebase',
+        'ngRoute'
+    ])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/chatroom/:id', {
-      templateUrl: 'app/chatroom/chatroom.html',
-      controller: 'ChatroomController'
-    });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/chatroom/:id', {
+                templateUrl: 'app/chatroom/chatroom.html',
+                controller: 'ChatroomController'
+            });
+    }])
 
-.controller('ChatroomController', ['$scope', '$firebaseObject', '$firebaseArray', '$routeParams',
-  function($scope, $firebaseObject, $firebaseArray, $routeParams) {
+    .controller('ChatroomController', ['$scope', '$firebaseObject', '$firebaseArray', '$routeParams',
+        function($scope, $firebaseObject, $firebaseArray, $routeParams) {
 
-  $scope.room = null;
+            $scope.room = null;
 
-  var refRoom = new Firebase('https://anonichat.firebaseio.com/chatrooms/' + $routeParams.id);
-  var syncRoom = $firebaseObject(refRoom);
-  syncRoom.$bindTo($scope, "room");
+            var refRoom = new Firebase('https://anonichat.firebaseio.com/chatrooms/' + $routeParams.id);
+            var syncRoom = $firebaseObject(refRoom);
+            syncRoom.$bindTo($scope, "room");
 
-  $scope.messages = $firebaseArray(refRoom.child('messages'));
+            $scope.messages = $firebaseArray(refRoom.child('messages'));
 
-  $scope.username = "You";
-  $scope.addMessage = function() {
-    $scope.messages.$add({
-      name: $scope.username,
-      message: $scope.message
-    });
-    $scope.message = "";
-  };
+            $scope.username = "You";
+            $scope.addMessage = function() {
+                $scope.messages.$add({
+                    name: $scope.username,
+                    message: $scope.message
+                });
+                $scope.message = "";
+                scrollChatToTop();
+            };
 
-  // for chat room GUI starts
 
+        }
+    ]);
 
-  // for chat room GUI ends
-
-}]);
+    function scrollChatToTop() {
+        var anchor = angular.element('.scroll-anchor')[0];
+        anchor.scrollIntoView();
+    }
+})();
